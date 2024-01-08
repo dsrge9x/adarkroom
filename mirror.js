4e0ae9748492b11a0ca8d6365323fcfb510164f7
dsrkafuu <dsrkafuu@outlook.com>
2024-01-08 19:55:33 +0800

    chore: remove audio

--- mirror.js
+++ mirror.js
@@ -11,7 +11,7 @@ fse.ensureDirSync(dist);
 fse.emptyDirSync(dist);
 
 const files = [
-  path.resolve(__dirname, './audio'),
+  // path.resolve(__dirname, './audio'),
   path.resolve(__dirname, './css'),
   path.resolve(__dirname, './img'),
   path.resolve(__dirname, './lang'),
